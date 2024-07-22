37e3e18751b2a099c7632a42713b5e4bbee1ca01
LorenzGartmeier <l.gartmeier@gmail.com>
2024-07-22 16:31:04 +0200

    introduce enum Perspective which indicates the ascii board's perspective

--- src/main/java/io/github/wolfraam/chessgame/board/visual/BoardVisualizer.java
+++ src/main/java/io/github/wolfraam/chessgame/board/visual/BoardVisualizer.java
@@ -2,8 +2,20 @@ package io.github.wolfraam.chessgame.board.visual;
 
 public class BoardVisualizer {
 
+  public enum Perspective {
+    WHITE,
+    BLACK,
+    CURRENT_PLAYER
+  }
 
-  public static String FenToBigAscii(String fen) {
+
+  public static final Perspective DEFAULT_PERSPECTIVE = Perspective.WHITE;
+
+  public static String fenToBigAscii(String fen) {
+    return fenToBigAscii(fen, DEFAULT_PERSPECTIVE);
+  }
+
+  public static String fenToBigAscii(String fen, Perspective perspective) {
     return null;
   }
 
